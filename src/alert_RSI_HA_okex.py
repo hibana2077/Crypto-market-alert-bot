9d3082d659ea5e3e203ff5bf54deb77d88216731
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-10-02 10:23:08 +0800

    Add files via upload

--- src/alert_RSI_HA_okex.py
+++ src/alert_RSI_HA_okex.py
@@ -15,7 +15,7 @@
 #  |______| |_|       \_____| |______|
                                     
                                     
-VERSION = '1.0.8'
+VERSION = '1.0.9'
 from pprint import pprint
 from talib import abstract
 import time , ccxt , requests , fake_useragent ,schedule
@@ -131,6 +131,14 @@ def combine_message(timeframe,over_buy_symbols,over_sell_symbols,keyword):
     '''
     return message
 
+def SMA(symbol,period):
+    day_data = okex.fetch_ohlcv(symbol, timeframe='1d', limit=period*2)
+    day_data = pd.DataFrame(day_data, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+    day_data['time'] = pd.to_datetime(day_data['time'], unit='ms')
+    day_data.set_index('time', inplace=True)
+    sma =abstract.SMA(day_data['close'],period,matype=0)
+    return '⬆️' if day_data['close'][-1] >= sma[-1] else '⬇️'
+
 def indicator(value,overbuy,oversell):return (True,'overbuy') if value >= overbuy else (True,'oversell') if value <= oversell else (False,'hold')
 
 def do(symbol , timeframe , limit , params:dict):
@@ -143,12 +151,13 @@ def do(symbol , timeframe , limit , params:dict):
         df['time'] = pd.to_datetime(df['time'],unit='ms')
         df.set_index('time',inplace=True)
         df['rsiHA_Open'],df['rsiHA_High'],df['rsiHA_Low'],df['rsiHA_Close'] = f_rsiHeikinAshi(limit,df['close'],df['high'],df['low'],i_smoothing=params['smooth_length'])
+        if df['rsiHA_Close'].iloc[-1] in (50,-50):continue
         status, trend = indicator(df['rsiHA_Close'][-1],overbuy=params['over_buy'],oversell=params['over_sell'])
         if status:
             if trend == 'overbuy':
-                over_buy_symbols.append(f"{t} : RSI_CLOSE {df['rsiHA_Close'][-1]:.3f} REAL_CLOSE {df['close'][-1]:.3f}")
+                over_buy_symbols.append(f"{t[:-10]} : RSI_CLOSE {df['rsiHA_Close'][-1]:.3f} {SMA(t,params['sma_period'])}")
             else:
-                over_sell_symbols.append(f"{t} : RSI_CLOSE {df['rsiHA_Close'][-1]:.3f} REAL_CLOSE {df['close'][-1]:.3f}")
+                over_sell_symbols.append(f"{t[:-10]} : RSI_CLOSE {df['rsiHA_Close'][-1]:.3f} {SMA(t,params['sma_period'])}")
         else:
             pass
     end = time.time()
@@ -180,13 +189,27 @@ def main(symbol_list,timeframe,length,params):
 
 def delay():
     s = time.time()
-    _ = okex.fetch_ticker('ADA/USDT:USDT')['last']
+    _ = okex.fetch_ohlcv('ADA/USDT:USDT',timeframe='1m',limit=1000)
     e = time.time()
     return f"{e-s:.3f}s"
 
 def sorted_by_trades(symbol_list):
     symbol_list.sort(key=lambda x:(okex.fetch_ohlcv(x,timeframe='1d',limit=2)[0][-1])*sum(okex.fetch_ohlcv(x,timeframe='1d',limit=2)[0][1:-1:])/4,reverse=True)
     return symbol_list
+
+#add this
+def daily_reflash():
+    global symbol_list
+    symbol_list = sorted_by_trades(symbol_list)
+
+def show_my_wallet():
+    print('''
+    ====== 打賞錢包 ======
+    USDT:
+    0x07311c9256CC01bD799b41C95a6dE7c09F4F12A1(BSC)
+    ADA:
+    DdzFFzCqrhswBxKbcqkm8no1x15mFEhfDwUhoMbdp7y1cARa25f9Pjmv6zhTsB5bKMhDUqRUDwj96Jfo7Z2s3tfqTtTdLTABt3DJYPis(Cardano)
+    =====================''')
 
 if __name__ == '__main__':
     print('=== 系統啟動 ===')
@@ -206,13 +229,16 @@ if __name__ == '__main__':
     over_buy = int(input("請輸入超買門檻:"))
     over_sell = int(input("請輸入超賣門檻:"))
     timeframe = int(input("請輸入時間框架:"))
+    sma_period = int(input("請輸入SMA週期:"))
     keyword = input("請輸入關鍵字:")
     programe_start_time = int(input("請輸入程式延遲啟動時間(如要立即啟動請輸入0)(分鐘):"))
     print(f'程式將在{programe_start_time}分鐘後啟動')
     time.sleep(programe_start_time*60)
     print('================')
     print('系統狀態: 啟動完成')
-    schedule.every(timeframe).minutes.at(":01").do(main,symbol_list=symbol_list,timeframe=timeframe,length=length,params={'smooth_length': smooth_length,'over_buy': over_buy,'over_sell': over_sell,'timeframe': timeframe,'keyword':keyword})
+    schedule.every().day.at("09:09").do(daily_reflash)#add this
+    schedule.every().saturday.at("09:09").do(show_my_wallet)#add this
+    schedule.every(timeframe).minutes.at(":01").do(main,symbol_list=symbol_list,timeframe=timeframe,length=length,params={'smooth_length': smooth_length,'over_buy': over_buy,'over_sell': over_sell,'timeframe': timeframe,'keyword':keyword,'sma_period':sma_period})
     while True:
         schedule.run_pending()
         time.sleep(1)
